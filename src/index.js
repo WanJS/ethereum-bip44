69ea3249f0bd7f1966c34984de7c84659830a5d7
Tyrion70 <tyrion70@gmail.com>
2018-09-14 09:20:39 +0200

    Add Circle and Standard

--- src/index.js
+++ src/index.js
@@ -1,67 +1,64 @@
-import { pubToAddress } from 'wanchainjs-util';
-import { HDPrivateKey, HDPublicKey } from 'bitcore-lib';
-import assert from 'assert';
-var ec = require('elliptic').ec('secp256k1');
+import { pubToAddress } from 'wanchainjs-util'
+import { HDPrivateKey, HDPublicKey } from 'bitcore-lib'
+import assert from 'assert'
+var ec = require('elliptic').ec('secp256k1')
 
-function padTo32(msg) {
+function padTo32 (msg) {
   while (msg.length < 32) {
-    msg = Buffer.concat([new Buffer([0]), msg]);
+    msg = Buffer.concat([Buffer.from([0]), msg])
   }
   if (msg.length !== 32) {
-    throw new Error(`invalid key length: ${msg.length}`);
+    throw new Error(`invalid key length: ${msg.length}`)
   }
-  return msg;
+  return msg
 }
 
 export default class EthereumBIP44 {
+  static fromPublicSeed (seed) {
+    return new EthereumBIP44(new HDPublicKey(seed))
+  }
 
-    static fromPublicSeed(seed) {
-        return new EthereumBIP44(new HDPublicKey(seed));
-    }
+  static fromPrivateSeed (seed) {
+    return new EthereumBIP44(new HDPrivateKey(seed))
+  }
 
-    static fromPrivateSeed(seed) {
-        return new EthereumBIP44(new HDPrivateKey(seed));
-    }
+  static bip32PublicToEthereumPublic (pubKey) {
+    let key = ec.keyFromPublic(pubKey).getPublic().toJSON()
+    return Buffer.concat([padTo32(Buffer.from(key[0].toArray())), padTo32(Buffer.from(key[1].toArray()))])
+  }
 
-    static bip32PublicToEthereumPublic(pubKey) {
-        let key = ec.keyFromPublic(pubKey).getPublic().toJSON();
-        return Buffer.concat([padTo32(new Buffer(key[0].toArray())), padTo32(new Buffer(key[1].toArray()))]);
-    }
+  constructor (hdKey) {
+    this.parts = [
+      `44'`, // bip 44
+      `60'`, // coin
+      `0'`, // wallet
+      `0` // 0 - public, 1 = private
+      // index
+    ]
 
-    constructor(hdKey) {
+    assert(hdKey)
 
-        this.parts = [
-            `44'`, // bip 44
-            `60'`,  // coin
-            `0'`,  // wallet
-            `0`    // 0 - public, 1 = private
-            // index
-        ];
+    this.key = hdKey
+  }
 
-        assert(hdKey);
+  derive (path) {
+    return this.key.derive(path)
+  }
 
-        this.key = hdKey;
-    }
+  getAddress (index) {
+    let path = this.parts.slice(this.key.depth)
+    let derived = this.key.derive('m/' + (path.length > 0 ? path.join('/') + '/' : '') + index)
+    let address = pubToAddress(
+      EthereumBIP44.bip32PublicToEthereumPublic(
+        derived.publicKey.toBuffer()
+      )
+    )
+    return `0x${address.toString('hex')}`
+  }
 
-    derive(path) {
-        return this.key.derive(path);
-    }
-
-    getAddress(index) {
-
-        let path = this.parts.slice(this.key.depth);
-        let derived = this.key.derive('m/' + (path.length > 0 ? path.join('/') + '/' : "") + index);
-        let address = pubToAddress(
-            EthereumBIP44.bip32PublicToEthereumPublic(
-                derived.publicKey.toBuffer()
-            )
-        );
-        return `0x${address.toString('hex')}`;
-    }
-
-    getPrivateKey(index) {
-      let path = this.parts.slice(this.key.depth);
-      let derived = this.key.derive('m/' + (path.length > 0 ? path.join('/') + '/' : "") + index);
-      return padTo32(derived.privateKey.toBuffer());
-    }
+  getPrivateKey (index) {
+    let path = this.parts.slice(this.key.depth)
+    let derived = this.key.derive('m/' + (path.length > 0 ? path.join('/') + '/' : '') + index)
+    return padTo32(derived.privateKey.toBuffer())
+  }
 }
